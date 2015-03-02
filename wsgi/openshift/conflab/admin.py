f29f2929bc0ba77a17b0d39c2796c3c6c6cf6e4d
Petr Kubat <xkubat11@stud.fit.vutbr.cz>
2015-03-02 13:34:59 +0100

    New models
    Signed-off-by: Petr Kubat <xkubat11@stud.fit.vutbr.cz>

--- wsgi/openshift/conflab/admin.py
+++ wsgi/openshift/conflab/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from conflab.models import *
 # Register your models here.
+
+admin.site.register(Room)
+admin.site.register(User)
+admin.site.register(EventType)
+admin.site.register(Event)
+admin.site.register(Paper)
