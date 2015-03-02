ff30df3c141d01e89de0bf9f6d53d144626c218c
Petr Kubat <xkubat11@stud.fit.vutbr.cz>
2015-03-02 10:45:28 +0100

    now able to run server locally

--- wsgi/openshift/manage.py
+++ wsgi/openshift/manage.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
-
+    # Add the root to the path to support local testing with
+    # runserver / WSGI_APPLICATION
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..','..'))
     execute_from_command_line(sys.argv)
