ff877bf2c4ebe7c1194ef695e979b1faf5d2c054
Petr Kubat <xkubat11@stud.fit.vutbr.cz>
2015-03-02 10:04:50 +0100

    added app to installed apps

--- wsgi/openshift/conflab/models.py
+++ wsgi/openshift/conflab/models.py
@@ -4,5 +4,5 @@ class Room(models.Model):
     s_name = models.CharField(max_length=20)
     name = models.CharField(max_length=256)
     desc = models.TextField()
-    color = models.ChaarField(max_length=8)
+    color = models.CharField(max_length=8)
     #conf = models.ForeignKey(Confs)
