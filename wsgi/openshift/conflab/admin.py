c497a7d51d5ee662ce162316ec5052178812afd0
Petr Kubat <xkubat11@stud.fit.vutbr.cz>
2015-03-02 15:12:53 +0100

    some changes to tables and display
    Signed-off-by: Petr Kubat <xkubat11@stud.fit.vutbr.cz>

--- wsgi/openshift/conflab/admin.py
+++ wsgi/openshift/conflab/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from conflab.models import *
 # Register your models here.
 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_start', 'topic')
+    #list_filter = ['event_start']
+
 admin.site.register(Room)
 admin.site.register(User)
 admin.site.register(EventType)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(Paper)
