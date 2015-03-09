425295d8218a53ede2206fcf8756ebf06c19d561
Petr Kubat <xkubat11@stud.fit.vutbr.cz>
2015-03-09 11:28:16 +0100

    added some details to user, event, conference models

--- wsgi/openshift/confla/admin.py
+++ wsgi/openshift/confla/admin.py
@@ -3,7 +3,7 @@ from confla.models import *
 # Register your models here.
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('event_start', 'topic')
+    list_display = ('event_start', 'topic', 'is_Scheduled')
     #list_filter = ['event_start']
 
 class RoomAdmin(admin.ModelAdmin):
@@ -11,13 +11,13 @@ class RoomAdmin(admin.ModelAdmin):
     list_display = ('id', 's_name')
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'name')
+    list_display = ('username', 'name', 'is_Speaker', 'is_Reviewer')
 
 class PaperAdmin(admin.ModelAdmin):
     list_display = ('user', 'title')
 
 class ConfAdmin(admin.ModelAdmin):
-    list_display = ('name', 'conf_start', 'conf_end')
+    list_display = ('name', 'conf_start', 'conf_end', 'is_Current')
 
 
 admin.site.register(Room, RoomAdmin)
@@ -25,4 +25,4 @@ admin.site.register(User, UserAdmin)
 admin.site.register(EventType)
 admin.site.register(Event, EventAdmin)
 admin.site.register(Paper, PaperAdmin)
-admin.site.register(Conf, ConfAdmin)
+admin.site.register(Conference, ConfAdmin)
--- wsgi/openshift/confla/models.py
+++ wsgi/openshift/confla/models.py
@@ -1,18 +1,23 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-class Conf(models.Model):
+class Conference(models.Model):
     name = models.CharField(max_length=256)
     conf_start = models.DateTimeField(blank=True, null=True)
     conf_end = models.DateTimeField(blank=True, null=True)
+    datetime.timedelta(days=1)
     def __str__(self):
         return self.name
+    def is_Current(self):
+        return self.conf_end > timezone.now()
 
 class Room(models.Model):
     s_name = models.CharField(max_length=16)
     name = models.CharField(max_length=64, blank=True)
     description = models.TextField(blank=True)
     color = models.CharField(max_length=8)
-    conf = models.ForeignKey(Conf)
+    conf = models.ForeignKey(Conference)
     def __str__(self):
         return self.s_name
 
@@ -33,6 +38,11 @@ class User(models.Model):
     bio = models.TextField (blank=True)
     def __str__(self):
         return self.username
+    def is_Speaker(self):
+        return Event.objects.filter(speaker=self.username) != [] 
+    def is_Reviewer(self):
+        return Paper.objects.filter(reviewer=self.username) != [] 
+    
 
 class EventType(models.Model):
     type_name = models.CharField(max_length=256)
@@ -40,11 +50,11 @@ class EventType(models.Model):
         return self.type_name
 
 class Event(models.Model):
-    conf = models.ForeignKey(Conf)
+    conf = models.ForeignKey(Conference)
     e_type = models.ForeignKey(EventType)
     event_start = models.DateTimeField(blank=True, null=True)
     event_end = models.DateTimeField(blank=True, null=True)
-    room = models.ForeignKey(Room, blank=True)
+    room = models.ForeignKey(Room, blank=True, null=True)
     topic = models.CharField(max_length=256)
     description = models.TextField()
     lang = models.CharField(max_length=6)
@@ -55,6 +65,8 @@ class Event(models.Model):
     volunteer = models.ManyToManyField(User, related_name='vol+', blank=True)
     def __str__(self):
         return self.topic
+    def is_Scheduled(self):
+        return (self.event_start != None) and (self.event_end != None) 
 
 class Paper(models.Model):
     user = models.ForeignKey(User)
