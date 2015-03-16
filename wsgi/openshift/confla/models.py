7e680316e31ea2dd0896217687988a4abd13dba8
Petr Kubat <xkubat11@stud.fit.vutbr.cz>
2015-03-16 11:21:01 +0100

    Changes to the database design
    Signed-off-by: Petr Kubat <xkubat11@stud.fit.vutbr.cz>

--- wsgi/openshift/confla/models.py
+++ wsgi/openshift/confla/models.py
@@ -5,9 +5,8 @@ from django.utils import timezone
 
 class Conference(models.Model):
     name = models.CharField(max_length=256)
-    conf_start = models.DateTimeField(blank=True, null=True)
-    conf_end = models.DateTimeField(blank=True, null=True)
-    datetime.timedelta(days=1)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -27,27 +26,26 @@ class Conference(models.Model):
         return event_set
 
 class Room(models.Model):
-    s_name = models.CharField(max_length=16)
+    shortname = models.CharField(max_length=16)
     name = models.CharField(max_length=64, blank=True)
     description = models.TextField(blank=True)
     color = models.CharField(max_length=8)
-    conf = models.ForeignKey(Conference)
 
     def __str__(self):
-        return self.s_name
+        return self.shortname
 
 class User(models.Model):
     username = models.CharField(max_length=32, primary_key=True) 
     name = models.CharField(max_length=32)
     phone = models.CharField(max_length=32, blank=True)
-    password = models.CharField(max_length=256)
-    #avatar = models.ImageField()
+    sha1 = models.CharField(max_length=256)
+    #picture = models.ImageField()
     company = models.CharField(max_length=256, blank=True)
     position = models.CharField(max_length=256, blank=True)
     web = models.URLField(max_length=512, blank=True)
     facebook = models.URLField(max_length=512, blank=True)
     twitter = models.URLField(max_length=512, blank=True)
-    google = models.URLField(max_length=512, blank=True)
+    google_plus = models.URLField(max_length=512, blank=True)
     linkedin = models.URLField(max_length=512, blank=True)
     bio = models.TextField (blank=True)
 
@@ -60,33 +58,32 @@ class User(models.Model):
     def is_Reviewer(self):
         return len(Paper.objects.filter(reviewer=self.username)) != 0 
 
-#    def is_Volunteer(self):
-#        return len(Paper.objects.filter(volunteer=self.username)) != 0     
-
 class VolunteerBlock(models.Model):
     name =  models.CharField(max_length=256)
-    start = models.DateTimeField(blank=True, null=True)
-    end = models.DateTimeField(blank=True, null=True)
-    
+    description = models.TextField(blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    room_id = models.ForeignKey(Room, blank=True, null=True)    
+    max_volunteers = models.IntegerField()
+
     def __str__(self):
         return self.name
 
-
 class Volunteer(models.Model):
     user = models.OneToOneField(User, primary_key=True)
-    block = models.ManyToManyField(VolunteerBlock, related_name='block+')
+    volunteer_block_id = models.ManyToManyField(VolunteerBlock, related_name='block+')
 
     def __str__(self):
         return self.user.username
 
 class Email(models.Model):
     user = models.ForeignKey(User)
-    email = models.EmailField(max_length=256)
-    active = models.BooleanField(default=False)
-    token = models.CharField(max_length=256)
+    address = models.EmailField(max_length=256)
+    is_active = models.BooleanField(default=False)
+    activation_token = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.email
+        return self.address
 
 class EventTag(models.Model):
     name = models.CharField(max_length=256)
@@ -95,43 +92,48 @@ class EventTag(models.Model):
         return self.name
 
 class EventType(models.Model):
-    type_name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.type_name
+        return self.name
 
 class Event(models.Model):
-    conf = models.ForeignKey(Conference)
-    e_type = models.ForeignKey(EventType)
-    event_start = models.DateTimeField(blank=True, null=True)
-    event_end = models.DateTimeField(blank=True, null=True)
-    room = models.ForeignKey(Room, blank=True, null=True)
+    conf_id = models.ForeignKey(Conference)
+    e_type_id = models.ForeignKey(EventType)
     topic = models.CharField(max_length=256)
     description = models.TextField()
     lang = models.CharField(max_length=6)
-    slide_url = models.URLField(max_length=512)
-    video_url = models.URLField(max_length=512, blank=True)
-    event_url = models.URLField(max_length=512)
+    slides = models.URLField(max_length=512)
+    video = models.URLField(max_length=512, blank=True)
     speaker = models.ManyToManyField(User, related_name='usr+')
     tags = models.ManyToManyField(EventTag, related_name='tag+')
 
     def __str__(self):
         return self.topic
 
+    #def is_Scheduled(self):
+    #    return (self.event_start != None) and (self.event_end != None) 
+
+class Timeslot(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    room_id = models.ForeignKey(Room, blank=True, null=True)
+    event_id = models.ForeignKey(Event, blank=True, null=True)
+    conf_id = models.ForeignKey(Conference)
+
     # event length in minutes
     def length(self):
-        if (self.event_start != None) and (self.event_end != None):
-            return round((self.event_end - self.event_start).seconds / 60)
+        if self.start_time and self.end_time:
+            return round((self.end_time - self.start_time).seconds / 60)
 
-    def is_Scheduled(self):
-        return (self.event_start != None) and (self.event_end != None) 
+   
 
 class Paper(models.Model):
     user = models.ForeignKey(User)
     title = models.CharField(max_length=256)
     abstract = models.TextField()
     #source = models.FileField()
-    accept = models.NullBooleanField()
+    accepted = models.NullBooleanField()
     reviewer = models.ManyToManyField(User, related_name='rev+')
 
     def __str__(self):
