49cbc51e2127efd36b82c6804e9e86052c906d78
Petr Kubat <xkubat11@stud.fit.vutbr.cz>
2015-07-06 13:02:06 +0200

    Added switching between grid and list user views
    Signed-off-by: Petr Kubat <xkubat11@stud.fit.vutbr.cz>

--- wsgi/openshift/confla/static/confla/userview.js
+++ wsgi/openshift/confla/static/confla/userview.js
@@ -1,4 +1,19 @@
 function changeView() {
     var sel = $(".sel-view option:selected").text();
-    if sel ===
+    switch (sel) {
+        case "List": {
+            $.get( "list", function( data ) {
+                $(".sched-wrap").html($(data).find(".schedlist-wrap").html());
+                $(".sched-wrap").removeClass().addClass("schedlist-wrap");
+            })
+            break; 
+        };
+        case "Grid": {
+            $.get( "", function( data ) {
+                $(".schedlist-wrap").html($(data).find(".sched-wrap").html());
+                $(".schedlist-wrap").removeClass().addClass("sched-wrap");
+            });
+            break;
+        }
+    } 
 }
