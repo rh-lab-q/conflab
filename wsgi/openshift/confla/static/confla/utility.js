3447a5e5d7bc8e3cfbea0a35dd91ebfc36f5c91c
Petr Kubat <xkubat11@stud.fit.vutbr.cz>
2016-02-01 13:29:14 +0100

    Filter events by tag in schedule
    Signed-off-by: Petr Kubat <xkubat11@stud.fit.vutbr.cz>

--- wsgi/openshift/confla/static/confla/utility.js
+++ wsgi/openshift/confla/static/confla/utility.js
@@ -17,6 +17,44 @@ function confla_toggle () {
     }
 }
 
+function toggle_events(tag) {
+    // Get the tag's class
+    $(this).toggleClass("tag-color");
+    var tag_name = $(this)[0].classList[0];
+    $(this).toggleClass("tag-color");
+    // Toggle event visibility for given tag
+    var items = $(".sched-wrap ." + tag_name).closest(".item");
+    if ($(this).hasClass("tag-disabled"))
+        items.show();
+    else
+        items.hide();
+    items.toggleClass("item-disabled");
+    // Hide empty rooms
+    $(".sched-wrap .table").each(function() {
+        var that = this;
+        // For each room name table header except the first one (time)
+        $("th:not(:first-child)", this).each(function() {
+            var index = $(this).index() + 1;
+            // If there are cells in a column given by "index" that dont have any events
+            if (!$("td:nth-child("+ index +") .item:not(.item-disabled)", that).length) {
+                $("td:nth-child("+ index +")", that).hide();
+                $(this).hide();
+            }
+            else {
+                $("td:nth-child("+ index +")", that).show();
+                $(this).show();
+            }
+        });
+    });
+    $(this).toggleClass("tag-disabled");
+}
+
+function legendInit() {
+    $("#legend-wrap .tag-color").each(function () {
+        $(this).click(toggle_events);
+    });
+}
+
 $(document).ready(function() {
     $(".confla-toggle").click(confla_toggle);
 })
